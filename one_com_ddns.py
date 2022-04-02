c8e3230a56ad65cd7743b9fb78e0b4e43a832df0
Luca Conte <main@lugico.de>
2022-04-02 15:25:17 +0200

    Include some error handling
    Wrong credentials and missing DNS records will now be caught by the script and handled accordingly.

--- one_com_ddns.py
+++ one_com_ddns.py
@@ -1,30 +1,32 @@
 '''
 
-    ###############################
-    ##                           ##
-    ##    one.com DDNS Script    ##
-    ##                           ##
-    ###############################
-    | Author       | Lugico       |
-    +--------------+--------------+
-    | Email        | me@lugico.de |
-    +--------------+--------------+
-    | Discord      | Lugico#4592  |
-    +--------------+--------------+
-    | Version      | 2.2          |
-    +--------------+--------------+
-    | Last Updated | 2021-10-20   |
-    +--------------+--------------+
+    #################################
+    ##                             ##
+    ##     one.com DDNS Script     ##
+    ##                             ##
+    #################################
+    | Version      | 2.3            |
+    +--------------+----------------+
+    | Last Updated | 2022-04-02     |
+    +--------------+----------------+
+    
+    +----------------+-------------------------+
+    | Initial Author | Lugico <main@lugico.de> |
+    +----------------+-------------------------+
+    | Contributors   | Vigge                   |
+    +----------------+-------------------------+
+    
+    
 
 
     Note:
         This script is not very fail proof.
-        something as little as a missing internet connection or a
-        slight amount of packet loss can cause the script to crash
-        or behave unexpectedly.
+        Very few possible exceptions are handled, something as simple
+        as a missing internet connection will make the script behave
+        unexpectedly. Use with caution.
 
-        I'm open to suggestions and will be glad to help if you
-        have trouble getting the script to work.
+        If you have any problems or suggestions, please open an issue
+        on github or send me an email (main@lugico.de)
 
 '''
 
@@ -87,7 +89,7 @@ if CHECK_IP_CHANGE:
 
     # write current ip to file
     with open(LAST_IP_FILE,"w") as f:
-        f.write(IP);
+        f.write(IP)
 
 
 def findBetween(haystack, needle1, needle2):
@@ -101,7 +103,7 @@ def loginSession(USERNAME,  PASSWORD, TARGET_DOMAIN=''):
     print("Logging in...")
 
     # create requests session
-    session = requests.session();
+    session = requests.session()
 
     # get admin panel to be redirected to login page
     redirectmeurl = "https://www.one.com/admin/"
@@ -114,9 +116,12 @@ def loginSession(USERNAME,  PASSWORD, TARGET_DOMAIN=''):
 
     # post login data
     logindata = {'username': USERNAME, 'password': PASSWORD, 'credentialId' : ''}
-    session.post(posturl, data=logindata)
+    response = session.post(posturl, data=logindata)
+    if response.text.find("Invalid username or password.") != -1:
+        print("!!! - Invalid credentials. Exiting")
+        exit(1)
 
-    print("Sent Login Data")
+    print("Login successful.")
 
     # For accounts with multiple domains it seems to still be needed to select which target domain to operate on.
     if TARGET_DOMAIN:
@@ -135,8 +140,10 @@ def selectAdminDomain(session, DOMAIN):
 def getCustomRecords(session, DOMAIN):
     print("Getting Records")
     getres = session.get("https://www.one.com/admin/api/domains/" + DOMAIN + "/dns/custom_records").text
-    #print(getres)
-    return json.loads(getres)["result"]["data"];
+    if len(getres) == 0:
+        print("!!! - No records found. Exiting")
+        exit()
+    return json.loads(getres)["result"]["data"]
 
 
 # finds the record id of a record from it's subdomain
@@ -175,4 +182,8 @@ records = getCustomRecords(s, DOMAIN)
 # loop through list of subdomains
 for subdomain in SUBDOMAINS:
     #change ip address
-    changeIP(s, findIdBySubdomain(records, subdomain), DOMAIN, subdomain, IP, 600)
+    recordid = findIdBySubdomain(records, subdomain)
+    if recordid == "":
+        print("!!! - Record '" + subdomain + "' could not be found.")
+        continue
+    changeIP(s, recordid, DOMAIN, subdomain, IP, 600)
